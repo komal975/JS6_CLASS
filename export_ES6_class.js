0a1d44dd98409c0a91ab4540412254e69ace3f79
Komal Bhise <komalbhise49@gmail.com>
2024-03-21 19:33:20 +0530

    Added files via Upload

---
+++ export_ES6_class.js
@@ -0,0 +1,4 @@
+export default function printStatement () {
+    let finalprint = console.log("I have a Ford, it is a Mustang."); 
+    return finalprint;
+}
---
+++ import_ES6_class.js
@@ -0,0 +1,3 @@
+import printStatement from './export_ES6_class.js';
+let finalprint = printStatement();
+console.log(finalprint);
